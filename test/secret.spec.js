6ade0ccc9c6dbc806323686e5df04f3751b263cc
Damien Laureaux <damien.laureaux@tuitravel.com>
2019-02-14 07:45:32 +0000

    Add missing unit-tests for the secret endpoints

---
+++ test/secret.spec.js
@@ -0,0 +1,65 @@
+const request = require('request');
+const { expect } = require('chai');
+const config = require('../src/config/config.json');
+
+// Configuration
+const id = 'test-01';
+const encryptionKey = 'p2s5v8y/B?E(H+MbQeShVmYq3t6w9z$C';
+const secret = {
+  first_name: 'firstname',
+  last_name: 'lastname',
+  email: 'email@email.com',
+  password: 'app123',
+  password_confirmation: 'app123',
+};
+
+
+// Add Secret
+describe('Add Secret endpoint response', () => {
+  it('should return 200', (done) => {
+    const options = {
+      method: 'POST',
+      url: `http://${config.app.host}:${config.app.port}/api/secrets`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        id,
+        encryption_key: encryptionKey,
+        value: secret,
+      },
+      json: true,
+    };
+
+    request(options, (err, res) => {
+      if (err) throw err;
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
+
+// Get Secret
+describe('Get Secret(s) endpoint response', () => {
+  it('should return 200', (done) => {
+    const options = {
+      method: 'POST',
+      url: `http://${config.app.host}:${config.app.port}/api/secrets/${id}`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        encryption_key: encryptionKey,
+      },
+      json: true,
+    };
+
+    request(options, (err, res) => {
+      if (err) throw err;
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
--- test/swagger.spec.js
+++ test/swagger.spec.js
@@ -2,7 +2,7 @@ const request = require('request');
 const { expect } = require('chai');
 const config = require('../src/config/config.json');
 
-// Server
+// Swagger
 describe('Swagger endpoint response', () => {
   it('should return 200', (done) => {
     request.get(`http://${config.app.host}:${config.app.port}/swagger/index.html`, (err, res) => {
