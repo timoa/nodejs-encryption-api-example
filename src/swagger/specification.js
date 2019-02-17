b8a021972700e3333e386facb8ad5014a163f2d0
Damien Laureaux <d.laureaux@timoa.com>
2019-02-17 18:59:55 +0000

    Update Swagger with examples

--- src/swagger/specification.js
+++ src/swagger/specification.js
@@ -8,10 +8,36 @@ exports.schema = {
         id: {
           type: 'string',
           description: 'Secret ID. You can also provide a wildcard "*" at the end of the secret ID',
+          example: 'test-01',
         },
         encryptionKey: {
           type: 'string',
           description: 'Encryption Key used to encrypt the secret before saving it to MongoDB',
+          example: 'p2s5v8y/B?E(H+MbQeShVmYq3t6w9z$C',
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'Default Response',
+        type: 'object',
+        properties: {
+          _id: {
+            type: 'string',
+            description: 'MongoDB ID',
+          },
+          id: {
+            type: 'string',
+            description: 'Secret ID',
+          },
+          __v: {
+            type: 'string',
+            description: 'MongoDB document version',
+          },
+          value: {
+            type: 'string',
+            description: 'Encrypted secret',
+          },
         },
       },
     },
@@ -25,14 +51,23 @@ exports.schema = {
         id: {
           type: 'string',
           description: 'Secret ID',
+          example: 'test-01',
         },
         encryptionKey: {
           type: 'string',
           description: 'Encryption Key used to encrypt the secret before saving it to MongoDB',
+          example: 'p2s5v8y/B?E(H+MbQeShVmYq3t6w9z$C',
         },
         value: {
           type: 'object',
           description: 'Object or any other JSON types to encrypt',
+          example: {
+            first_name: 'firstname',
+            last_name: 'lastname',
+            email: 'email@email.com',
+            password: 'app123',
+            password_confirmation: 'app123',
+          },
         },
       },
     },
