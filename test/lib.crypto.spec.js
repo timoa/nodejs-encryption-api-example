0204383ae45313c379816e7acd496f5dd8cefb02
Damien Laureaux <d.laureaux@timoa.com>
2019-02-23 16:46:38 +0000

    Add Logger and Crypto libraries tests

--- test/lib.crypto.spec.js
+++ test/lib.crypto.spec.js
@@ -13,7 +13,7 @@ const unencrypted = {
 const encrypted = '0f80f3912cb30fc0e69c90f6cbf5b79f:906e26046549891c947ff2bac234a24a0a5bbbae448104f8e5ffca710e00629aad1af5dcc03bb09a022441e2f1508609e38b693f574f4229e4d5a6af06a1c816edf06f524b5822fe3f4ed8ccb6b36ad9d73780aeaa885dfad0f9e51a690b986acf7e0219cff6918a0822516a390e803a19a0c3a909f1a55d4d5ef251539e75bed40ddbb4f8a9fb627b3a5c176de96011';
 
 // Crypto
-describe('Crypto', () => {
+describe('Crypto library', () => {
   it('expect the "encrypt" function to return a string', (done) => {
     expect(crypto.encrypt(JSON.stringify(unencrypted), encryptionKey)).to.be.a('string');
     done();
---
+++ test/lib.logger.spec.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const logger = require('../src/lib/logger');
+
+// Logger
+describe('Logger library', () => {
+  it('expect the "logger.info" function to be a function', (done) => {
+    expect(typeof (logger.info)).to.be.equals('function');
+    done();
+  });
+  it('expect the "logger.info" function to not throw without an argument', (done) => {
+    expect(logger.info).to.not.throw();
+    done();
+  });
+  it('expect the "logger.debug" function to be a function', (done) => {
+    expect(typeof (logger.debug)).to.be.equals('function');
+    done();
+  });
+  it('expect the "logger.debug" function to not throw without an argument', (done) => {
+    expect(logger.debug).to.not.throw();
+    done();
+  });
+  it('expect the "logger.warn" function to be a function', (done) => {
+    expect(typeof (logger.warn)).to.be.equals('function');
+    done();
+  });
+  it('expect the "logger.warn" function to not throw without an argument', (done) => {
+    expect(logger.warn).to.not.throw();
+    done();
+  });
+  it('expect the "logger.error" function to be a function', (done) => {
+    expect(typeof (logger.error)).to.be.equals('function');
+    done();
+  });
+  it('expect the "logger.error" function to not throw without an argument', (done) => {
+    expect(logger.error).to.not.throw();
+    done();
+  });
+});
